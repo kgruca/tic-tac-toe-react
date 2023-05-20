3e503294d3bca1716a0765a6b52bcf37c14f04aa
kgruca <kgruca21@gmail.com>
2023-05-19 18:30:34 -0700

    user can travel to any point in the game's history

--- src/App.js
+++ src/App.js
@@ -55,10 +55,12 @@ const Game = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   };
 
